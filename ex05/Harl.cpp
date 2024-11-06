b38e0d024a1540c96cee324e20d69bdcaa3b0fb9
Zouhirborali <zouhirbakkas@gmail.com>
2024-11-06 18:26:59 +0100

    ex05

---
+++ ex05/Harl.cpp
@@ -0,0 +1,41 @@
+
+#include "Harl.hpp"
+
+
+void Harl::debug()
+{
+    std::cout << "debug";
+}
+void Harl::info()
+{
+    std::cout << "info";
+}
+void Harl::warning()
+{
+    std::cout << "warning";
+}
+void Harl::error()
+{
+    std::cout << "error";
+}
+
+void Harl::complain(std::string level)
+{
+
+    //Array of function pointers to private member functions
+    void(Harl::*cont[4])() = {&Harl::debug,&Harl::info,&Harl::warning,&Harl::error};
+
+    std::string str[4] ={"DEBUG","INFO","WARNING","ERROR"};
+    int  i =0;
+    while (str[i]!=level)
+    {
+        i++;
+    }
+    if(i<4 && level==str[i])
+    {
+        (this->*cont[i])();
+        return;
+    }
+    std::cout << "bad arg";
+
+}
---
+++ ex05/Harl.hpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zbakkas <zouhirbakkas@gmail.com>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/06 15:23:45 by zbakkas           #+#    #+#             */
+/*   Updated: 2024/11/06 15:23:49 by zbakkas          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#include <iostream>
+
+class Harl
+{
+    private:
+        void debug( void );
+        void info( void );
+        void warning( void );
+        void error( void );
+    public:
+        void complain( std::string level );
+};
+
